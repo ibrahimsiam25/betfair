a86a8a058527c31cc40e6d1deb5f510c78b741a5
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2024-10-24 22:20:52 +0300

    done new fear two

--- lib/core/widgets/custom_text_feild.dart
+++ lib/core/widgets/custom_text_feild.dart
@@ -2,22 +2,21 @@ import 'dart:ui';
 import 'package:betfair/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 class CustomTextField extends StatelessWidget {
-  final Function(String) onChanged; 
-  final Icon? icon; // Change to Icon type for flexibility
+  final Function(String) onChanged;
+  final Widget? icon;
 
   const CustomTextField({
     super.key,
-    required this.onChanged, 
-    this.icon, // Accept an optional icon parameter
+    required this.onChanged,
+    this.icon,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 50.h,
+      height: 50,
       decoration: BoxDecoration(
         color: AppColors.darkPrimary.withOpacity(0.5),
         borderRadius: const BorderRadius.all(
@@ -43,15 +42,22 @@ class CustomTextField extends StatelessWidget {
             child: TextField(
               decoration: InputDecoration(
                 border: InputBorder.none,
-                contentPadding:const EdgeInsets.symmetric(horizontal: 20),
-                suffixIcon: icon, // Place the icon here
+                contentPadding: icon != null
+                    ? const EdgeInsets.fromLTRB(20, 15, 0, 15) // Adjust padding when icon is present
+                    : const EdgeInsets.symmetric(horizontal: 20),
+                suffixIcon: icon != null
+                    ? Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: icon,
+                      )
+                    : null,
               ),
               style: const TextStyle(
                 color: Colors.white,
                 fontSize: 18,
               ),
               cursorColor: AppColors.darkPrimary,
-              onChanged: onChanged, // Use the callback here
+              onChanged: onChanged,
             ),
           ),
         ),
--- lib/features/fears/widgets/new_fears_two_view_body.dart
+++ lib/features/fears/widgets/new_fears_two_view_body.dart
@@ -1,13 +1,99 @@
+import 'package:betfair/core/constants/app_assets.dart';
+import 'package:betfair/features/fears/widgets/new_fear_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 
-class NewFearsTwoViewBody extends StatelessWidget {
-  const NewFearsTwoViewBody({super.key, required this.fears,required this.howLong});
-final String fears ;
-final String howLong;
+import '../../../core/theme/app_text_styles.dart';
+import '../../../core/widgets/custom_button.dart';
+import '../../../core/widgets/custom_text_feild.dart';
+import '../../../core/widgets/show_custom_snack_bar.dart';
+
+class NewFearsTwoViewBody extends StatefulWidget {
+  const NewFearsTwoViewBody(
+      {super.key, required this.fears, required this.howLong});
+  final String fears;
+  final String howLong;
+
+  @override
+  State<NewFearsTwoViewBody> createState() => _NewFearsTwoViewBodyState();
+}
+
+class _NewFearsTwoViewBodyState extends State<NewFearsTwoViewBody> {
+  String textFieldValueOne = '';
+  String textFieldValueTwo = '';
+  void _onNextPressed() async {
+    if (textFieldValueOne.isEmpty || textFieldValueTwo.isEmpty) {
+      showCustomSnackBar(
+        context,
+        "Please enter all fields",
+      );
+    } else {
+      // GoRouter.of(context).push(
+      //   AppRouter.kNewTwoFears,
+      //   extra: {
+      //     kFears: textFieldValue,
+      //     kHowLong: buttonLabels[selectedDurationIndex],
+      //   },
+      // );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 5.w),
+      child: Column(
+        children: [
+          const NewFearAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 28.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 16.h),
+                Text(
+                  "Date of the start of the fight",
+                  style: AppTextStyles.fontWhiteW400.copyWith(fontSize: 16.sp),
+                ),
+                SizedBox(height: 3.h),
+                CustomTextField(
+                  icon: SvgPicture.asset(
+                    Assets.imagesCalendar,
+                    width: 20,
+                    height: 20,
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      textFieldValueOne = value;
+                    });
+                  },
+                ),
+                SizedBox(height: 15.h),
+                 Text(
+                  "What control methods were used",
+                  style: AppTextStyles.fontWhiteW400.copyWith(fontSize: 16.sp),
+                ),
+                SizedBox(height: 3.h),
+                CustomTextField(
+
+                  onChanged: (value) {
+                    setState(() {
+                      textFieldValueTwo = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+          const Spacer(),
+          CustomButton(
+            text: "NEXT",
+            onPressed: _onNextPressed,
+          ),
+          SizedBox(height: 16.h),
+        ],
+      ),
     );
   }
 }
