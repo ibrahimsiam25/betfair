47760f07479328115be96bfdcb77aa55c73d00b5
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2024-10-25 15:53:36 +0300

    Adjust SituationPageView height to 58% of screen height
    The height of the `SituationPageView` widget was previously set to 60% of the screen height. This commit adjusts the height to 58% to better fit the content and improve

--- lib/features/test/widgets/situation_page_view.dart
+++ lib/features/test/widgets/situation_page_view.dart
@@ -18,7 +18,7 @@ class SituationPageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.6,
+      height: MediaQuery.of(context).size.height * 0.58,
       child: PageView.builder(
         physics: const NeverScrollableScrollPhysics(),
         controller: pageController,
