94b00888b6b291f797c30346ed109aed8b7c2098
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2024-10-25 14:35:40 +0300

    create  final situtaion view

---
+++ lib/features/test/widgets/situation_final_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SituationFinalViewBody extends StatelessWidget {
+  const SituationFinalViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
