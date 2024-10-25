2373b12561a156f425bd0772528e15c349771121
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2024-10-25 15:39:34 +0300

    ref

--- lib/features/test/widgets/situation_save_view_body.dart
+++ lib/features/test/widgets/situation_save_view_body.dart
@@ -9,67 +9,76 @@ import 'package:go_router/go_router.dart';
 
 import '../../../core/functions/get_color_for_rate.dart';
 import '../../../core/theme/app_colors.dart';
-
-class SituationSaveViewBody extends StatelessWidget {
+class SituationSaveViewBody extends StatelessWidget { 
   const SituationSaveViewBody({super.key, required this.rateOfSituation});
   final List<int> rateOfSituation;
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25.0),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 15.h,
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.8,
-            child: ListView.builder(
-              itemCount: situationData.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  margin: const EdgeInsets.only(bottom: 26.0),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Situation ${index + 1}\n${situationData[index].title}",
-                          style: AppTextStyles.fontWhiteW700
-                              .copyWith(fontSize: 20.sp),
-                        ),
-                        Container(
-                          width: 50.w,
-                          height: 40.h,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage(Assets.imagesNumberback),
-                              fit: BoxFit.fill,
-                            ),
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 15.h,
+                ),
+                // Directly include ListView.builder without Expanded
+                ListView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: situationData.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.only(bottom: 26.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Situation ${index + 1}\n${situationData[index].title}",
+                            style: AppTextStyles.fontWhiteW700.copyWith(fontSize: 20.sp),
                           ),
-                          child: Center(
-                            child: Text(
-                              rateOfSituation[index].toString(),
-                              style: AppTextStyles.fontWhiteW700.copyWith(
-                                fontSize: 22.sp,
-                                color: getColorForRate(rateOfSituation[index]),
+                          Container(
+                            width: 50.w,
+                            height: 40.h,
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage(Assets.imagesNumberback),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                            child: Center(
+                              child: Text(
+                                rateOfSituation[index].toString(),
+                                style: AppTextStyles.fontWhiteW700.copyWith(
+                                  fontSize: 22.sp,
+                                  color: getColorForRate(rateOfSituation[index]),
+                                ),
                               ),
                             ),
                           ),
-                        )
-                      
-                      ]),
-                );
-              },
+                        ],
+                      ),
+                    );
+                  },
+                ), 
+             
+              ],
             ),
-          ), const Spacer(flex: 2,),
-            CustomButton(
-              fontColor: AppColors.theme,
-              text: "SAVE", onPressed: (){
-                GoRouter.of(context).push(AppRouter.kSituationFinal);
-              }),
-               const Spacer(flex: 1,),
-        ],
-      ),
+          ),
+        ),
+       const   Spacer(),
+                CustomButton(
+                  fontColor: AppColors.theme,
+                  text: "SAVE",
+                  onPressed: () {
+                    GoRouter.of(context).push(AppRouter.kSituationFinal);
+                  },
+                ),
+                SizedBox(height: 10.h,)
+      ],
     );
   }
 }
