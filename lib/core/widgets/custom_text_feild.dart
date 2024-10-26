1e00bdb8f08ea03a0daab67fe3211fd9d4f20dd8
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2024-10-26 21:53:59 +0300

    Refactor CustomTextField to support date selection and update dependencies
    - Converted `CustomTextField` from a stateless to a stateful widget to manage internal state and handle date selection.
    - Added a `TextEditingController` to manage the text field's content and update it with selected dates.
    - Implemented a `_selectDate` method to show a date picker dialog with a custom theme and update the text field with the formatted date.
    - Updated the `TextField` widget to be read-only when an icon is present, allowing it to trigger the date picker on tap.
    - Adjusted padding for the `TextField` based on the presence of an icon.
    - Added the `intl` package to handle date formatting and updated `pubspec.yaml` and `pubspec.lock` to reflect this dependency change.

--- lib/core/widgets/custom_text_feild.dart
+++ lib/core/widgets/custom_text_feild.dart
@@ -1,17 +1,51 @@
 import 'dart:ui';
-
 import 'package:betfair/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   final Function(String) onChanged;
   final Widget? icon;
 
   const CustomTextField({
-    super.key,
+    Key? key,
     required this.onChanged,
     this.icon,
-  });
+  }) : super(key: key);
+
+  @override
+  _CustomTextFieldState createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  final TextEditingController _controller = TextEditingController();
+
+  Future<void> _selectDate(BuildContext context) async {
+    DateTime? selectedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now(),
+       builder: (BuildContext context, Widget? child) {
+        return Theme(
+          data: ThemeData.light().copyWith(
+            primaryColor:AppColors.primary,
+          cardColor: AppColors.primary,
+          canvasColor: AppColors.primary,
+            colorScheme:const ColorScheme.light(primary: AppColors.darkPrimary),
+            buttonTheme:const ButtonThemeData(textTheme: ButtonTextTheme.primary), // Button color
+          ),
+          child: child!,
+        );
+      },
+    );
+
+    if (selectedDate != null) {
+      String formattedDate = DateFormat('yyyy-MM-dd').format(selectedDate);
+      _controller.text = formattedDate; // Update the text field with the formatted date
+      widget.onChanged(formattedDate); // Call the onChanged function
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,16 +75,16 @@ class CustomTextField extends StatelessWidget {
           child: Container(
             color: Colors.black.withOpacity(0.2),
             child: TextField(
+              controller: _controller, // Use the stateful controller
               decoration: InputDecoration(
                 border: InputBorder.none,
-                contentPadding: icon != null
-                    ? const EdgeInsets.fromLTRB(
-                        20, 15, 0, 15) // Adjust padding when icon is present
+                contentPadding: widget.icon != null
+                    ? const EdgeInsets.fromLTRB(20, 15, 0, 15)
                     : const EdgeInsets.symmetric(horizontal: 20),
-                suffixIcon: icon != null
+                suffixIcon: widget.icon != null
                     ? Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: icon,
+                        child: widget.icon,
                       )
                     : null,
               ),
@@ -59,7 +93,11 @@ class CustomTextField extends StatelessWidget {
                 fontSize: 18,
               ),
               cursorColor: AppColors.darkPrimary,
-              onChanged: onChanged,
+              onChanged: widget.onChanged,
+              readOnly: widget.icon != null,
+              onTap: widget.icon != null
+                  ? () => _selectDate(context) // No longer need to pass controller
+                  : null,
             ),
           ),
         ),
